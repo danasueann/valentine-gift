eee28af8dd011aefea24c385ec49d920139226dc
dana.guensche <dana.guensche@ausbildung.diakonie-am-campus.de>
2025-02-10 13:47:55 +0100

    test

---
+++ src/components/Letter.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-card class="mx-auto" max-width="700">
+        <v-img color="surface-variant" height="200" src="https://cdn.vuetifyjs.com/docs/images/cards/purple-flowers.jpg"
+            cover>
+        </v-img>
+
+        <v-card-text>
+            <v-card-title class="headline">Mein Juli,</v-card-title>
+            <v-card-text>
+<p> mit diesem kleinen Brief möchte ich mich von Herzen bei dir bedanken. Danke, dass du mir zeigst, was es bedeutet, wirklich geschätzt zu werden.Die Art und Weise, wie du lachst und schmunzelst, wenn ich dir ein Kompliment gebe, dir sage, dass ich dich liebe, zeigt mir, dass es dir wirklich etwas bedeutet. Es ist einfach wunderschön anzusehen, dich glücklich zu sehen. Ich sage es dir vielleicht oft, aber ich habe manchmal das Gefühl, du könntest denken, es wären nur dahingesagte, schnulzige Worte. Aber glaub mir, Juli, wenn ich dir sage, dass ich es wirklich zu 100% ernst meine, immer. Dein Lächeln verzaubert mich, deine Art und Weise, so voller Liebe und Ruhe, deine Stimme ist einfach zuckersüß. Es ist wahnsinnig attraktiv, dass du in meinem Lieblingsspiel einfach der Beste bist den ich kenne, ich schaue einfach zu dir hinauf und will nur ein bisschen so sein wie du. Du bist in meinen Augen einfach das liebenswerteste Etwas, das ich kenne :3 Einfach knuffig.Ich bin so gespannt, was die Zukunft zwischen uns bringen wird. Auch wenn ich manchmal unsicher bin, verspreche ich dir, dass ich mein Bestes geben werde, dich jeden Tag glücklich zu machen und dir zu zeigen, wie wertvoll du für mich bist.Es ist wirklich verrückt, wie du einfach den ganzen Tag in meinem Kopf bist. Ich wache auf und denke an dich, stelle mir dich vor. Ich sitze in der Arbeit, schaue aus dem Fenster und stelle mir vor, dass du aus einem der Autos aussteigst und einfach bei mir bist. Wie ich abends einschlafe und mir vorstelle, du wärst bei mir, wie ich dich küsse, dich berühren kann...Mann, Juli! Als wir uns das erste Mal gesehen haben, war ich so aufgeregt, weil du einfach der schönste Mensch bist, dem ich je gegenüberstand. Und dann warst du auch noch meinetwegen da? Hallo? D: Was ist da los? Deine Augen haben in dem wunderschönsten Blau gestrahlt, das ich mir vorstellen kann. Wie soll man da keine weichen Knie bekommen? Ich hätte mir nicht mal im Entferntesten vorstellen können, dass so ein Mann zu mir fährt, mir wunderschöne Blumen schenkt, mir Komplimente macht, mit mir kuschelt und mich einfach mag. Ich fühle mich einfach unglaublich geehrt, das erleben zu dürfen. Dass du auf mich wartest, dass du MICH liebst, dass du nur mit mir sein möchtest. Dieses Gefühl mit dir lässt mich wieder strahlen, und dafür möchte ich dir von ganzem Herzen danken.
+Deine Evangeline (Dana) :3</p>
+            </v-card-text>
+        </v-card-text>
+    </v-card>
+</template>
+<script>
+export default {
+    name: 'Letter',
+    data: () => ({
+        
+    }),
+}
+</script>
---
+++ src/components/Welcome.vue
@@ -0,0 +1,14 @@
+<template>
+    <Letter></Letter>
+</template>
+
+<script>
+import Letter from './Letter.vue'
+export default {
+    name: 'Welcome',
+
+    components: {
+        Letter
+    },
+}
+</script>
